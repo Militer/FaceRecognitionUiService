e3dd2f8cfc1b41baebe879b49cab2a378e1e9b0d
Gabriel Militaru <militaru@hpe.com>
2017-06-22 08:15:18 +0300

    Renamed the application as face-recognition ui-service

--- src/main/resources/public/app/core/core.module.js
+++ src/main/resources/public/app/core/core.module.js
@@ -3,5 +3,5 @@
  */
 angular
     .module('core', ['ngResource'])
-    .constant('CAR_ENGINE_BASE_URL', '/car-service/car-engine')
-    .constant('SERVER_BASE_URL', '/car-service');
+    .constant('CAR_ENGINE_BASE_URL', '/face-recognition-image-service/car-engine')
+    .constant('SERVER_BASE_URL', '/face-recognition-image-service');
